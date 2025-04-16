a0ee56f91c94187af665cd33d3f456d855d4f8de
Ur_7nw <auliarizasyahputraa@gmail.com>
2025-04-16 19:55:20 +0700

    nyawa sudah
    tinggal tampilan utama sama levelnya

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+public class PlayerCollision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Enemy")
+        {
+            HealtManager.health--;
+            if (HealtManager.health <= 0){
+                 PlayerManager.isGameOver = true;
+                 gameObject.SetActive(false);
+            }else{
+                StartCoroutine(GetHurt());
+            }
+
+        }
+    }
+
+    IEnumerator GetHurt(){
+        Physics2D.IgnoreLayerCollision(6,8);
+        GetComponent<Animator>().SetLayerWeight(1, 1);
+        yield return new WaitForSeconds(3);
+        GetComponent<Animator>().SetLayerWeight(1, 0);
+        Physics2D.IgnoreLayerCollision(6,8, false);
+    }
+}
